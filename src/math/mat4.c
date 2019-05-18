040f1008292e5b65c9a98bf0ed4244eadc94a256
zachwildd <zacharyjwild@gmail.com>
2019-05-18 11:43:39 -0400

    Add mat4_identity

---
+++ src/math/mat4.c
@@ -0,0 +1,54 @@
+/**
+ * @file mat4.c
+ * @author Zach Wild (www.opensourceryumd.com)
+ * @brief 
+ * @version 0.1
+ * @date 2019-05-16
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
+
+#include "vec3.h"
+#include "mat4.h"
+
+/**
+ * @brief Returns the 4x4 identity matrix
+ * 
+ * @return Mat4   4x4 identity matrix
+ */
+Mat4 mat4_identity()
+{
+  int row = 0;
+  int col = 0;
+
+  Mat4 *mat = malloc(sizeof(Mat4));                               // alloc mat4
+  mat->rows = calloc(MAT4_ROWS, sizeof(rt_float_t *));           // alloc rows
+
+  while (row < MAT4_ROWS) 
+  {
+
+    mat->rows[row] = calloc(MAT4_COLS, sizeof(rt_float_t * ));  // alloc cols
+
+    col = 0;
+    while (col < MAT4_COLS) 
+    {
+      if (col == row) 
+      {
+        mat->rows[row][col] = 1.0;
+      }
+      col += 1;
+    }
+
+    row += 1;
+  }
+
+  return *mat;
+}
+
+Mat4 mat4_translation(Vec3);
+Mat4 mat4_rotation_x(Vec3);
+Mat4 mat4_rotation_y(Vec3);
+Mat4 mat4_rotation_z(Vec3);
+Mat4 mat4_rotation_x(Vec3);
+Mat4 mat4_translate(Mat4, Vec3);
--- src/math/mat4.h
+++ src/math/mat4.h
@@ -9,13 +9,17 @@
  * 
  */
 
+#ifndef __MAT_4_H__
+#define __MAT_4_H__
+
 #include "../common.h"
 #include "vec3.h"
 
-#define MAT4_SIZE 16
+#define MAT4_COLS 4
+#define MAT4_ROWS 4
 
 typedef struct {
-	rt_float_t **cells
+	rt_float_t **rows;
 } Mat4;
 
 Mat4 mat4_identity();
@@ -25,3 +29,5 @@ Mat4 mat4_rotation_y(Vec3);
 Mat4 mat4_rotation_z(Vec3);
 Mat4 mat4_rotation_x(Vec3);
 Mat4 mat4_translate(Mat4, Vec3);
+
+#endif
