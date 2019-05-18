038d9e650067d70740e6f68224d12780a4f52d47
zachwildd <zacharyjwild@gmail.com>
2019-05-18 11:44:26 -0400

    Fix comments

--- src/math/mat4.c
+++ src/math/mat4.c
@@ -22,13 +22,13 @@ Mat4 mat4_identity()
   int row = 0;
   int col = 0;
 
-  Mat4 *mat = malloc(sizeof(Mat4));                               // alloc mat4
+  Mat4 *mat = malloc(sizeof(Mat4));                              // alloc mat4
   mat->rows = calloc(MAT4_ROWS, sizeof(rt_float_t *));           // alloc rows
 
   while (row < MAT4_ROWS) 
   {
 
-    mat->rows[row] = calloc(MAT4_COLS, sizeof(rt_float_t * ));  // alloc cols
+    mat->rows[row] = calloc(MAT4_COLS, sizeof(rt_float_t * ));   // alloc cols
 
     col = 0;
     while (col < MAT4_COLS) 
